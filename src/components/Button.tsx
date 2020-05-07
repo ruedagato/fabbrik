bcf5c4e498e1a89a4a90c1ecb1999300594a7ced
Jose Vargas <ruedagato@gmail.com>
2020-05-07 14:08:38 -0500

    chage styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Button } from 'primereact/button';
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
 
 const CButton = () => <Button label="holaa 22" />;
 
